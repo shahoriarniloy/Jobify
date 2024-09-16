b8e06f276a29cd7319162b8e8f824a2b4723cf4d
shahoriarniloy <niloyshahoriar@gmail.com>
2024-09-17 02:57:52 +0600

    New Branch Created

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,6 +3,7 @@ const Home = () => {
     return (
         <div>
             <h1 className="text-red-600">This Is Home</h1>
+            <h1>Development Branch</h1>
             
         </div>
     );
