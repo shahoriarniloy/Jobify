c817f3be0c587e9b0c2870453d1373d880dba6c9
Abdullah-Az-Zahur <abdullah.az.zahur@gmail.com>
2024-09-17 13:49:52 +0600

    Creat : Company details page

---
+++ src/Pages/CompanyDetails/CompanyDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CompanyDetails = () => {
+    return (
+        <div>
+            <h2>Company Details</h2>
+        </div>
+    );
+};
+
+export default CompanyDetails;
