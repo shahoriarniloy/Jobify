35d7e7ce59159a25062d846578ae478faba6a8e0
bella908 <mumtahina efa8@gmail.com>
2024-09-17 15:44:39 +0600

    added basic navbar and colour scheme

--- src/Pages/Shared/Navbar.jsx
+++ src/Pages/Shared/Navbar.jsx
@@ -1,10 +1,66 @@
+import { Link } from "react-router-dom";
+import logo from '../../assets/logo.png'
 
 const Navbar = () => {
+    const navItem =
+        <>
+            <li className='text-brownText'><Link to='/'>Home</Link></li>
+            <li className='text-brownText' ><Link to='/'>Find job</Link></li>
+            <li className='text-brownText'><Link to='/'>Company Search</Link></li>
+        </>
     return (
         <div>
-            <h1>This is Navbar</h1>
-            
+        <div className="navbar bg-transparent fixed top-0 left-0 right-0 z-50">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </div>
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                        {navItem}
+                    </ul>
+                </div>
+                <div>
+
+
+                    <img className='h-20 w-52' src={logo} alt="" />
+
+                </div>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className=" text-white  gap-7 menu-horizontal px-1">
+                    {navItem}
+                </ul>
+            </div>
+            <div className="navbar-end">
+            {/* {
+  user? <>
+      <span></span>
+  <button onClick={handleSignOut} className="btn btn-outline border-white text-white mx-2">Log Out</button>
+
+  <div className="avata " >
+<div className="w-12" >
+<div className="tooltip tooltip-bottom" data-tip={user.displayName}> 
+
+<img src={user.photoURL} className="rounded-full" />
+
+</div>
+</div>
+</div>
+  </>
+  :
+  <div className="navbar-end gap-5">
+<Link to="/logIn">
+<a className="btn btn-outline border-white text-white">Log-In</a>
+</Link>
+
+    </div>
+
+
+} */}
+            </div>
         </div>
+    </div>
     );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,23 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-  ],  theme: {
-    extend: {},
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brownlogo: '#693705', // 
+        brownText: '#593B34', 
+        greenCastomize: '#1e6a6e',
+        yellowCastomize: '#F5CF82', 
+        blueCastomize: '#629BAD', 
+        redCastomize: '#C67265', 
+        // 
+      },
+    },
   },
-  plugins: [require('daisyui'),],daisyui:{
-    themes:['light']},
-}
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: ['light'],
+  },
+};
 
