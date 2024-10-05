7002b7982c6fb9488a08132a4f53e3527c494d99
Abdullah-Az-Zahur <abdullah.az.zahur@gmail.com>
2024-10-06 01:15:42 +0600

    Add : use react select for make add social media input

--- src/Pages/Dashboard/Employee/Components/DragAndDropInput.jsx
+++ src/Pages/Dashboard/Employee/Components/DragAndDropInput.jsx
@@ -58,7 +58,7 @@ const DragAndDropInput = ({ type, label, file, onFileUpload }) => {
       <label className="block mb-2 font-bold">{label}</label>
       <div
         {...getRootProps()}
-        className={`p-6 rounded-md cursor-pointer transition ${isDragActive ? "bg-blue-100 border-blue-400" : "bg-white"}`}
+        className={`p-6 rounded-md cursor-pointer transition ${isDragActive ? "bg-blue-100 border-blue-400" : "bg-gray-300"}`}
         style={{ minHeight: "150px" }}
         onClick={() => fileInputRef.current.click()} // Ensure the file input opens on click
       >
