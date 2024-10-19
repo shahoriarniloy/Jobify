c57d76a160b27400df17f76d6ed1086322c30d66
Abdullah-Az-Zahur <abdullah.az.zahur@gmail.com>
2024-10-19 23:33:24 +0600

    trans : company - post a job

--- src/Pages/Dashboard/Company/PostJob.jsx
+++ src/Pages/Dashboard/Company/PostJob.jsx
@@ -171,7 +171,7 @@ const PostJob = () => {
                 <div className="md:col-span-5">
                   <div className="flex gap-4">
                     <div className="w-1/3">
-                      <label htmlFor="salaryRange">Salary Range</label>
+                      <label htmlFor="salaryRange">{t("salary_range")}</label>
                       <select
                         required
                         name="salaryRange"
@@ -179,7 +179,7 @@ const PostJob = () => {
                         onChange={handleChange}
                         className="h-10 border mt-1 rounded px-4 w-full bg-gray-50 p-2"
                       >
-                        <option value="">Select Salary Range</option>
+                        <option value="">{t("select_salary_range")}</option>
                         <option value="$500-$1000">$500-$1000</option>
                         <option value="$1000-$2000">$1000-$2000</option>
                         <option value="$2000-$3000">$2000-$3000</option>
@@ -191,6 +191,7 @@ const PostJob = () => {
                         <option value="$15000+">$15000+</option>
                       </select>
                     </div>
+
                     <div className="w-1/3">
                       <label htmlFor="location">Location</label>
                       <input
@@ -205,14 +206,14 @@ const PostJob = () => {
                     </div>
 
                     <div className="md:col-span-5">
-                      <label htmlFor="vacancy">Vacancy</label>
+                      <label htmlFor="vacancy">{t("vacancy")}</label>
                       <input
                         type="number"
                         name="vacancy"
                         value={jobData.vacancy}
                         onChange={handleChange}
                         className="h-10 border mt-1 rounded px-4 w-full bg-gray-50 p-2"
-                        placeholder="Number of vacancies"
+                        placeholder={t("placeholder_number_of_vacancies")}
                         required
                         min="1"
                       />
@@ -222,7 +223,7 @@ const PostJob = () => {
 
                 <div className="md:col-span-5 flex gap-4">
                   <div>
-                    <label htmlFor="jobLevel">Job Level</label>
+                    <label htmlFor="jobLevel">{t("job_level")}</label>
                     <select
                       required
                       name="jobLevel"
@@ -230,15 +231,17 @@ const PostJob = () => {
                       onChange={handleChange}
                       className="h-10 border mt-1 rounded px-4 w-full bg-gray-50 p-2"
                     >
-                      <option value="">Select Job Level</option>
-                      <option value="Entry Level">Entry Level</option>
-                      <option value="Mid Level">Mid Level</option>
-                      <option value="Expert Level">Expert Level</option>
+                      <option value="">{t("select_job_level")}</option>
+                      <option value="Entry Level">{t("entry_level")}</option>
+                      <option value="Mid Level">{t("mid_level")}</option>
+                      <option value="Expert Level">{t("expert_level")}</option>
                     </select>
                   </div>
 
                   <div>
-                    <label htmlFor="deadline">Application Deadline</label>
+                    <label htmlFor="deadline">
+                      {t("application_deadline")}
+                    </label>
                     <input
                       type="date"
                       name="deadline"
@@ -253,7 +256,7 @@ const PostJob = () => {
                 <div className="md:col-span-5">
                   <div className="flex gap-4 mb-4">
                     <div className="w-1/3">
-                      <label htmlFor="education">Education</label>
+                      <label htmlFor="education">{t("education")}</label>
                       <select
                         required
                         name="education"
@@ -261,16 +264,23 @@ const PostJob = () => {
                         onChange={handleChange}
                         className="h-10 border mt-1 rounded px-4 w-full bg-gray-50 p-2"
                       >
-                        <option value="">Select Education</option>
-                        <option value="High School">High School</option>
-                        <option value="Intermediate">Intermediate</option>
-                        <option value="Graduation">Graduation</option>
-                        <option value="Bachelor Degree">Bachelor Degree</option>
-                        <option value="Master Degree">Master Degree</option>
+                        <option value="">{t("select_education")}</option>
+                        <option value="High School">{t("high_school")}</option>
+                        <option value="Intermediate">
+                          {t("intermediate")}
+                        </option>
+                        <option value="Graduation">{t("graduation")}</option>
+                        <option value="Bachelor Degree">
+                          {t("bachelor_degree")}
+                        </option>
+                        <option value="Master Degree">
+                          {t("master_degree")}
+                        </option>
                       </select>
                     </div>
+
                     <div className="w-1/3">
-                      <label htmlFor="jobType">Job Type</label>
+                      <label htmlFor="jobType">{t("job_type")}</label>
                       <select
                         required
                         name="jobType"
@@ -278,41 +288,43 @@ const PostJob = () => {
                         onChange={handleChange}
                         className="h-10 border mt-1 rounded px-4 w-full bg-gray-50 p-2"
                       >
-                        <option value="">Select Job Type</option>
-                        <option value="All">All</option>
-                        <option value="Full Time">Full Time</option>
-                        <option value="Part Time">Part Time</option>
-                        <option value="Internship">Internship</option>
-                        <option value="Remote">Remote</option>
-                        <option value="Temporary">Temporary</option>
-                        <option value="Contract Based">Contract Based</option>
+                        <option value="">{t("select_job_type")}</option>
+                        <option value="All">{t("all")}</option>
+                        <option value="Full Time">{t("full_time")}</option>
+                        <option value="Part Time">{t("part_time")}</option>
+                        <option value="Internship">{t("internship")}</option>
+                        <option value="Remote">{t("remote")}</option>
+                        <option value="Temporary">{t("temporary")}</option>
+                        <option value="Contract Based">
+                          {t("contract_based")}
+                        </option>
                       </select>
                     </div>
                   </div>
                 </div>
 
                 <div className="md:col-span-5">
-                  <label htmlFor="jobDescription">Job Description</label>
+                  <label htmlFor="jobDescription">{t("job_description")}</label>
                   <textarea
                     name="jobDescription"
                     value={jobData.jobDescription}
                     onChange={handleChange}
                     className="h-20 border mt-1 rounded px-4 w-full bg-gray-50 p-2"
-                    placeholder="Enter job description"
+                    placeholder={t("enter_job_description")}
                     required
                   />
                 </div>
 
                 <div className="md:col-span-5">
                   <label htmlFor="responsibilities">
-                    Responsibilities (one per line)
+                    {t("responsibilities")}
                   </label>
                   <textarea
                     name="responsibilities"
                     value={jobData.responsibilities}
                     onChange={handleChange}
                     className="h-20 border mt-1 rounded px-4 w-full bg-gray-50 p-2"
-                    placeholder="Enter job responsibilities (e.g., Collect and test water samples)"
+                    placeholder={t("enter_job_responsibilities")}
                     required
                   />
                 </div>
@@ -323,7 +335,7 @@ const PostJob = () => {
                     className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded flex items-center"
                   >
                     <PaperAirplaneIcon className="h-5 w-5 mr-2" />
-                    Post Job
+                    {t("post_job")}
                   </button>
                 </div>
               </div>
