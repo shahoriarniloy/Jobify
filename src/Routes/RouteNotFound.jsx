e251a72be5b62f6cb3c6701d9ecab4228fe86b71
Abdullah-Az-Zahur <abdullah.az.zahur@gmail.com>
2024-10-23 19:48:13 +0600

    transl : route not found

--- src/Routes/RouteNotFound.jsx
+++ src/Routes/RouteNotFound.jsx
@@ -1,9 +1,12 @@
+import { useTranslation } from "react-i18next";
 
 const RouteNotFound = () => {
+  const { t } = useTranslation();
+  
   return (
     <div>
       <h1 className="flex justify-center items-center text-red-600">
-        Page Not Found
+        {t("page_not_found")}
       </h1>
     </div>
   );
