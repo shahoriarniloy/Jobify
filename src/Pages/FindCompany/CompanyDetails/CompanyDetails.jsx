910bb167013a9c5e7a1845ad625235c6b5d1d561
indraghosh02 <indraghosh0802@gmail.com>
2024-10-24 17:30:20 +0600

    completed fin company details

--- src/Pages/FindCompany/CompanyDetails/CompanyDetails.jsx
+++ src/Pages/FindCompany/CompanyDetails/CompanyDetails.jsx
@@ -28,6 +28,9 @@ import axiosSecure from "../../../Hooks/UseAxiosSecure.jsx";
 import ButtonLoader from "../../../Shared/ButtonLoader.jsx";
 import { toast } from "react-toastify";
 import useCurrentUser from "../../../Hooks/useCurrentUser.jsx";
+import { useSelector } from "react-redux";
+
+
 
 const CompanyDetails = () => {
   const { currentUser } = useCurrentUser();
@@ -35,6 +38,7 @@ const CompanyDetails = () => {
   const { companyId } = useParams();
   const { t } = useTranslation();
   const [isFavorite, setIsFavorite] = useState(false);
+  const theme = useSelector((state) => state.theme.theme);
 
   const companyEmail = company?.email;
   const userEmail = currentUser?.email;
@@ -108,7 +112,7 @@ const CompanyDetails = () => {
   };
 
   return (
-    <div className="bg-secondary">
+    <div className={theme === "dark"? "bg-gradient-to-r from-gray-800 to-slate-900" : "bg-secondary"}>
       <div className="relative container mx-auto">
         <div className="relative">
           <div>
@@ -118,7 +122,7 @@ const CompanyDetails = () => {
               alt={t("company_banner_alt")}
             />
           </div>
-          <div className="container absolute left-1/2 transform -translate-x-1/2 md:-bottom-16 bg-white rounded-lg shadow-lg p-4 md:p-6 lg:p-8 w-11/12 md:w-3/4 lg:w-1/2">
+          <div className={theme === "dark"? "container absolute left-1/2 transform -translate-x-1/2 md:-bottom-16 bg-slate-900 rounded-lg shadow-lg p-4 md:p-6 lg:p-8 w-11/12 md:w-3/4 lg:w-1/2" :"container absolute left-1/2 transform -translate-x-1/2 md:-bottom-16 bg-white rounded-lg shadow-lg p-4 md:p-6 lg:p-8 w-11/12 md:w-3/4 lg:w-1/2"}>
             <div className="flex flex-col md:flex-row items-center">
               {company?.company_logo ? (
                 <img
@@ -180,13 +184,13 @@ const CompanyDetails = () => {
             <h2 className="font-bold lg:mt-2 text-xl md:text-2xl lg:text-3xl">
               {t("description")}
             </h2>
-            <p className="text-gray-500 mb-4">{company?.company_description}</p>
+            <p className={theme === "dark"? "text-gray-300 mb-4" : "text-gray-500 mb-4"}>{company?.company_description}</p>
 
             <h2 className="font-bold my-5 text-xl md:text-2xl lg:text-3xl">
               {t("company_vision")}
             </h2>
-            <p className="text-gray-500 mb-4">{t("benefits_intro")}</p>
-            <ul className="list-disc text-gray-500 ml-5">
+            <p className= {theme === "dark"? "text-gray-300 mb-4" : "text-gray-500 mb-4"}>{t("benefits_intro")}</p>
+            <ul className= {theme === "dark"? "list-disc text-gray-300 mb-4 " : "list-disc text-gray-500 mb-4"} >
               <li>{t("benefit_1")}</li>
               <li>{t("benefit_2")}</li>
               <li>{t("benefit_3")}</li>
@@ -225,11 +229,12 @@ const CompanyDetails = () => {
                 </Link>
               </div>
             </div>
-            <div className="p-4 md:p-8 border-2 rounded-lg grid lg:grid-cols-2 md:grid-cols-2 grid-cols-1 gap-6 md:gap-10 mt-8">
+            <div className={`p-4 md:p-8 border-2 rounded-lg grid lg:grid-cols-2 md:grid-cols-2 grid-cols-1 gap-6 md:gap-10 mt-8 ${theme === "dark" ? "border-slate-600" : ""}`}>
+
               <div className="flex items-center gap-2">
                 <FiCalendar className="text-2xl text-blue-500" />
                 <div>
-                  <p className="text-gray-500 ">
+                  <p className={ theme==="dark"? "text-gray-300 " : "text-gray-500 "}>
                     {t("founded_in")} {company?.founded_date}
                   </p>
                 </div>
@@ -237,53 +242,53 @@ const CompanyDetails = () => {
               <div className="flex items-center gap-2">
                 <BiStopwatch className="text-2xl text-blue-500" />
 
-                <p className="font-bold text-sm text-gray-500">
+                <p className= { theme==="dark"? " text-gray-300 " : "font-bold text-sm text-gray-500"}>
                   {company?.company_type} {t("company")}
                 </p>
               </div>
               <div className="flex items-center gap-2">
                 <PiWallet className="text-2xl text-blue-500" />
-                <p className="text-gray-500 ">{t("team_size")}:</p>
-                <p className="font-bold text-sm text-gray-500">
+                <p className={ theme==="dark"? "text-gray-300 " : "text-gray-500 "}>{t("team_size")}:</p>
+                <p className={ theme==="dark"? "font-bold text-sm text-gray-300 " : "font-bold text-sm text-gray-500"} >
                   {company?.company_size} {t("candidates")}
                 </p>
               </div>
               <div className="flex items-center gap-2">
                 <PiBriefcase className="text-2xl text-blue-500" />
-                <p className="text-gray-500 ">{t("industry_types")}</p>
-                <p className="font-bold text-sm text-gray-500">
+                <p className={ theme==="dark"? "text-gray-300 " : "text-gray-500 "}>{t("industry_types")}</p>
+                <p className={ theme==="dark"? "font-bold text-sm text-gray-300 " : "font-bold text-sm text-gray-500"}>
                   {company?.industry}
                 </p>
               </div>
             </div>
 
-            <div className="p-4 md:p-8 border-2 rounded-lg md:my-6">
+            <div className={theme === 'dark'? "p-4 md:p-8 border-2 border-slate-600 rounded-lg md:my-6": "p-4 md:p-8 border-2 rounded-lg md:my-6"}>
               <h2 className="font-bold text-xl md:text-2xl">
                 {t("contact_information")}
               </h2>
               <div className="flex items-center my-5">
                 <FiGlobe className="text-2xl text-blue-500" />
                 <div className="ml-4">
-                  <p className="text-gray-500">
+                  <p className={ theme==="dark"? "text-gray-300 " : "text-gray-500 "}>
                     {t("website")}:{company?.company_website}
                   </p>
                 </div>
               </div>
-              <hr />
+              <hr className={theme === "dark"? "border-gray-500": ""}/>
               <div className="flex items-center my-5">
                 <LuPhoneCall className="text-2xl text-blue-500" />
                 <div className="ml-4">
-                  <p className="text-gray-500">{t("phone")}</p>
+                  <p className={ theme==="dark"? "text-gray-300 " : "text-gray-500 "}>{t("phone")}</p>
                   <p className="text-black font-bold">
                     {company?.phone_number}
                   </p>
                 </div>
               </div>
-              <hr />
+              <hr className={theme === "dark"? "border-gray-500": ""}/>
               <div className="flex items-center mt-5">
                 <TfiEmail className="text-2xl text-blue-500" />
                 <div className="ml-4">
-                  <p className="text-gray-500">
+                  <p className={ theme==="dark"? "text-gray-300 " : "text-gray-500 "}>
                     {t("email")}: {company?.email}
                   </p>
                 </div>
