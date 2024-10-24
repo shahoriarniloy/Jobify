03f76f6070ebc6152f5d2d38962f452f3cf96d86
bella908 <mumtahina efa8@gmail.com>
2024-10-24 12:25:56 +0600

    title about

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -2,10 +2,14 @@ import Intro from "./Intro";
 import Feedback from "./Feedback";
 import WhyChooseUs from "./WhyChooseUs";
 import OurTeam from "./OurTeam/OurTeam";
+import { Helmet } from "react-helmet";
 
 const About = () => {
   return (
     <div className="bg-secondary">
+        <Helmet>
+        <title>Jobify - About</title>
+      </Helmet>
       <Intro />
       <WhyChooseUs />
       <OurTeam />
