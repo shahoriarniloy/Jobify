9a868c64073adf3a23fe3dd3579af8f7b7bb1b86
bella908 <mumtahina efa8@gmail.com>
2024-10-24 12:16:43 +0600

    titel home

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,10 +4,15 @@ import HowItWorks from "./HowItWorks";
 import OurTeam from "../About/OurTeam/OurTeam";
 import PopularCategory from "./PopularCategory";
 import TopCompanies from "./TopCompanies";
+import { Helmet } from "react-helmet";
 // import TopCompanies from "./TopCompanies"
 const Home = () => {
   return (
     <div>
+        <Helmet>
+        <title>Jobify - Home</title>
+        <meta name="description" content="Explore jobs, top companies, popular categories, and how Jobify works." />
+      </Helmet>
       <SearchBar></SearchBar>
       <PopularCategory/>
       <HowItWorks></HowItWorks>
