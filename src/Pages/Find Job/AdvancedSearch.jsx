b11b39307ca49f9e7bd153c19fd0ad371642e210
Abdullah-Az-Zahur <abdullah.az.zahur@gmail.com>
2024-10-26 00:35:38 +0600

    transl : advanced search

--- src/Pages/Find Job/AdvancedSearch.jsx
+++ src/Pages/Find Job/AdvancedSearch.jsx
@@ -147,9 +147,8 @@ const AdvancedSearch = () => {
                 className={
                   theme === "dark"
                     ? "w-full pl-12 pr-3 py-3 sm:py-4 bg-slate-900 text-white rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-300 ease-in-out"
-                    : "w-full pl-12 pr-3 py-3 sm:py-4 bg-white  rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-300 ease-in-out"
+                    : "w-full pl-12 pr-3 py-3 sm:py-4 bg-white rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-300 ease-in-out"
                 }
-               
               />
             </div>
 
@@ -372,7 +371,11 @@ const AdvancedSearch = () => {
           <div className="flex items-center justify-center lg:gap-4 md:gap-4 gap-2 mt-4">
             <label
               htmlFor="itemsPerPage"
-              className={theme === "dark" ? "text-sm font-medium text-gray-300" : "text-sm font-medium text-blue-900"}
+              className={
+                theme === "dark"
+                  ? "text-sm font-medium text-gray-300"
+                  : "text-sm font-medium text-blue-900"
+              }
             >
               {t("number_of_jobs_per_page")}
             </label>
@@ -411,15 +414,18 @@ const AdvancedSearch = () => {
         </div>
 
         {viewMode === "list" ? (
-          <div className=" p-2 sm:p-4 dark:text-gray-800 mt-4">
+          <div className="p-2 sm:p-4 dark:text-gray-800 mt-4">
             <div className="overflow-x-auto">
               <div className="overflow-x-auto">
                 <table className="min-w-full text-xs sm:text-sm">
                   <thead className="dark:bg-gray-300">
                     <tr className="text-left">
                       <th className="p-3">{t("job_title")}</th>
-                      <th className="p-3">Vacancy</th>
-                      <th className="p-3 hidden md:table-cell">Job Type</th>
+                      <th className="p-3">{t("vacancy")}</th>
+                      <th className="p-3 hidden md:table-cell">
+                        {t("job_type")}
+                      </th>
+
                       <th className="p-3 hidden md:table-cell">
                         {t("salary")}
                       </th>
@@ -466,7 +472,7 @@ const AdvancedSearch = () => {
                         <td className="p-3">
                           <Link to={`/job/${_id}`}>
                             <button className="btn btn-outline btn-info btn-sm">
-                              View Details
+                              {t("view_details")}
                             </button>
                           </Link>
                         </td>
@@ -487,9 +493,13 @@ const AdvancedSearch = () => {
             {jobs?.map(({ _id, jobInfo, companyInfo }) => (
               <div
                 key={_id}
-                className={ theme === "dark" ? " w-full relative group cursor-pointer overflow-hidden bg-slate-700 bg-opacity-50 px-6  py-8 ring-1 ring-gray-900/5 transition-all duration-300  sm:mx-auto sm:max-w-sm sm:rounded-lg sm:px-10 hover:scale-95" : " w-full relative group cursor-pointer overflow-hidden bg-white px-6  py-8 ring-1 ring-gray-900/5 transition-all duration-300  sm:mx-auto sm:max-w-sm sm:rounded-lg sm:px-10 hover:scale-95"}
+                className={
+                  theme === "dark"
+                    ? " w-full relative group cursor-pointer overflow-hidden bg-slate-700 bg-opacity-50 px-6  py-8 ring-1 ring-gray-900/5 transition-all duration-300  sm:mx-auto sm:max-w-sm sm:rounded-lg sm:px-10 hover:scale-95"
+                    : " w-full relative group cursor-pointer overflow-hidden bg-white px-6  py-8 ring-1 ring-gray-900/5 transition-all duration-300  sm:mx-auto sm:max-w-sm sm:rounded-lg sm:px-10 hover:scale-95"
+                }
               >
-                <span className="absolute top-10 z-0 h-20 w-20 rounded-full  duration-300 "></span>
+                <span className="absolute top-10 z-0 h-20 w-20 rounded-full duration-300"></span>
                 <div className="relative z-10 mx-auto max-w-md">
                   <span className="grid size-[60px] place-items-center rounded-full ">
                     <img
@@ -497,7 +507,13 @@ const AdvancedSearch = () => {
                       className="h-full w-full rounded-full transition-all"
                     />
                   </span>
-                  <div className={ theme === "dark" ? "pt-5 text-base  text-gray-300 transition-all duration-300 " : "pt-5 text-base  text-gray-600 transition-all duration-300 "}>
+                  <div
+                    className={
+                      theme === "dark"
+                        ? "pt-5 text-base  text-gray-300 transition-all duration-300 "
+                        : "pt-5 text-base  text-gray-600 transition-all duration-300 "
+                    }
+                  >
                     <h2 className="text-2xl font-semibold tracking-wide flex gap-2">
                       {jobInfo?.title}
                       <div className="p-2 rounded-full text-xs bg-[#1d4fd83a] size-[28px] flex justify-center items-center">
@@ -506,27 +522,40 @@ const AdvancedSearch = () => {
                     </h2>
                     <p className="font-semibold">{companyInfo?.company_name}</p>
                     <p className="text-sm tracking-wide mt-3">
-                      <span className="font-semibold">Category: </span>
+                      <span className="font-semibold">
+                        {t("category_label")}
+                      </span>
+
                       {jobInfo?.jobCategory}
                     </p>
                     <p className="text-sm tracking-wide mt-1">
-                      <span className="font-semibold">Job Type: </span>
+                      <span className="font-semibold">
+                        {t("job_type_label")}
+                      </span>
                       {jobInfo?.jobType}
                     </p>
                     <p className="text-sm mt-1">
-                      <span className="font-semibold">Salary Range : </span>
+                      <span className="font-semibold">
+                        {t("salary_range_label")}
+                      </span>
                       {jobInfo?.salaryRange}
                     </p>
                     <p className="text-sm mt-1">
-                      <span className="font-semibold">Job Level : </span>
+                      <span className="font-semibold">
+                        {t("job_level_label")}
+                      </span>
                       {jobInfo?.jobLevel}
                     </p>
                     <p className="text-sm mt-1">
-                      <span className="font-semibold">Deadline : </span>
+                      <span className="font-semibold">
+                        {t("deadline_label")}
+                      </span>
                       {jobInfo?.deadline}
                     </p>
                     <p className="text-sm mt-1">
-                      <span className="font-semibold">Location : </span>
+                      <span className="font-semibold">
+                        {t("location_label")}
+                      </span>
                       {jobInfo?.location}
                     </p>
 
@@ -539,7 +568,11 @@ const AdvancedSearch = () => {
                   <div className="pt-5 text-base font-semibold leading-7">
                     <Link
                       to={`/job/${_id}`}
-                      className= { theme === "dark" ? "text-gray-200 transition-all duration-300  flex items-center" :"text-slate-500 transition-all duration-300  flex items-center"}
+                      className={
+                        theme === "dark"
+                          ? "text-gray-200 transition-all duration-300  flex items-center"
+                          : "text-slate-500 transition-all duration-300  flex items-center"
+                      }
                     >
                       {t("view_details")}
                     </Link>
