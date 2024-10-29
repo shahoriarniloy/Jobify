7d695907519e089aeaa1aacec1d62f10e1f2deb2
md-maruf-billa <157400283+md-maruf-billa@users.noreply.github.com>
2024-10-30 02:24:47 +0600

    redesign footer and update logo for this web

--- src/Shared/Footer.jsx
+++ src/Shared/Footer.jsx
@@ -10,43 +10,68 @@ const Footer = () => {
 
   return (
     <footer
-      className={
-        theme === "dark"
-          ? "footer footer-center bg-slate-900 text-slate-300 rounded p-10"
-          : "footer footer-center bg-white text-base-content rounded p-10"
-      }
+      className="bg-[#18191c] text-slate-300 rounded pt-[100px] pb-10 px-10"
+
     >
-      <nav className="grid grid-flow-col gap-4">
-        <Link to="/about">
-          <a className="link link-hover">{t("about_us")}</a>
-        </Link>
-        <Link to="/about">
-          <a className="link link-hover">{t("contact")}</a>
-        </Link>
-        <Link to="/advanced-search">
-          <a className="link link-hover">{t("jobs")}</a>
-        </Link>
-        <Link to="/companies">
-          <a className="link link-hover">{t("companies")}</a>
-        </Link>
-      </nav>
-      <nav>
-        <div className="grid grid-flow-col gap-4">
-          <a
-            href="https://github.com/shahoriarniloy/Jobify"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FaGithub />
-          </a>
+      <div className="container mx-auto grid md:grid-cols-3 gap-6 md:gap-0 lg:grid-cols-4">
+
+        <div >
+          <div className="flex items-center">
+            <img className="size-[70px]" src="./logo.png" alt="" />
+            <h2 className="text-3xl font-semibold text-[#0a65cc]">Jobify</h2>
+          </div>
+
+          <div className="space-y-2">
+            <h2><span className="text-gray-400">Call Now :</span> <span className=" font-semibold">+880 01730827996</span></h2>
+            <h2><span className="text-gray-400">E-mail :</span> <span className=" font-semibold">jobify@gmail.com</span></h2>
+            <h3 className="text-gray-400">Kurigram sodor, Kurigram<br />
+              Rangpur, Bangladesh
+            </h3>
+          </div>
+
         </div>
-      </nav>
-      <aside>
-        <p>
-          {t("copyright")} © {new Date().getFullYear()} -{" "}
-          {t("all_rights_reserved")}
-        </p>
-      </aside>
+
+        <div className="flex md:justify-end">
+          <div className="flex flex-col gap-5 *:text-gray-400">
+            <h3 className="text-xl font-semibold !text-white">Quick Link</h3>
+            <Link>About</Link>
+            <Link>Contact</Link>
+            <Link>Blog</Link>
+            <Link>Pricing</Link>
+          </div>
+        </div>
+
+        <div className="flex md:justify-end">
+          <div className="flex flex-col gap-5 *:text-gray-400">
+            <h3 className="text-xl font-semibold !text-white">Navigate Now</h3>
+            <Link>Browse Jobs</Link>
+            <Link>Browse Employers</Link>
+            <Link>Candidate Dashboard</Link>
+            <Link>Saved Jobs</Link>
+          </div>
+        </div>
+
+        <div className="flex lg:justify-end">
+          <div className="flex flex-col gap-5 *:text-gray-400">
+            <h3 className="text-xl font-semibold !text-white">Support</h3>
+            <Link>Faqs</Link>
+            <Link>Privacy Policy</Link>
+            <Link>Terms & Conditions</Link>
+          </div>
+        </div>
+
+      </div>
+
+      {/* lower footer */}
+      <hr className="border-1 mt-4" />
+
+      <div className="container mx-auto flex justify-between items-center mt-4">
+        <h3>@ 2024 Jobify - Job Portal. All rights Reserved</h3>
+
+        <div>
+          <a><FaGithub className="text-2xl" /></a>
+        </div>
+      </div>
     </footer>
   );
 };
